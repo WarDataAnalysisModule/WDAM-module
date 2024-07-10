a261b300d8a223bd0e952d014459b500caf939d8
Soyoung Lee <soo7132@naver.com>
2024-07-10 17:47:00 +0900

    특성 1, 2, 5 수정

--- module1.py
+++ module1.py
@@ -68,12 +68,9 @@ return: 추출한 로그
 """
 def Extract_UnitSpeed(id, user_idx, log_created, cursor):
 
-#     print("ID:", id)
-#     print("User ID:", user_idx)
-#     print("Log Created:", log_created)
-
+    # 관측 시간, 위치, 속도 추출
     query = "SELECT simulation_time, position_lat, position_lon, position_alt, speed FROM unit_attributes WHERE list_idx = %s AND created_at=%s AND user_idx=%s"
-    cursor.execute(query, (id, log_created, user_idx))
+    cursor.execute(query, (id,log_created,user_idx))
     result=cursor.fetchall()
 
     # 추출한 데이터로부터 데이터프레임 생성
@@ -86,9 +83,38 @@ def Extract_UnitSpeed(id, user_idx, log_created, cursor):
         text=f"{row['SimulationTime']} \t {row['positionLat']} \t {row['positionLon']} \t {row['positionAlt']} \t {row['speed']}"
         input_texts.append(text)
 
-    input_texts='\n'.join(input_texts)
     return input_texts
 
+"""
+brief: 데이터 길이가 길 때 적당한 크기로 자르는 함수
+split_input_texts_2는 2개로 자르고
+split_input_texts_3는 3개로 자른다.
+"""
+def split_input_texts_2(input_texts):
+    header = input_texts[0]  # 첫 번째 줄은 헤더
+    split_index = 150  # 헤더 포함 150번째 인덱스를 기준으로 분할 (즉, 150번째 데이터 행 이후)
+    
+    input_texts1 = input_texts[:split_index]
+    input_texts2 = [header] + input_texts[split_index:]
+    
+    return input_texts1, input_texts2
+
+def split_input_texts_3(input_texts):
+    header = input_texts[0]  # 첫 번째 줄은 헤더
+    total_lines = len(input_texts)
+    
+    # 헤더를 제외한 데이터의 시작 인덱스
+    data_start_index = 1
+
+    # 데이터를 3등분하기 위한 인덱스 계산
+    part1_end_index = data_start_index + (total_lines - data_start_index) // 3
+    part2_end_index = part1_end_index + (total_lines - data_start_index) // 3
+
+    input_texts1 = input_texts[:part1_end_index]
+    input_texts2 = [header] + input_texts[part1_end_index:part2_end_index]
+    input_texts3 = [header] + input_texts[part2_end_index:]
+    
+    return input_texts1, input_texts2, input_texts3
 ###################################################################################################
 
 """
@@ -100,51 +126,70 @@ param4: 데이터베이스 커서
 return: 추출한 로그
 """
 def Extract_Event(id, user_idx, log_created, cursor):
-
-#     print("ID:", id)
-#     print("User ID:", user_idx)
-#     print("Log Created:", log_created)
-
     # init 파일에서 초기 상태를 추출
-    query1 = "SELECT unit_name, status, member, equipment, supply FROM unit_init WHERE list_idx = %s AND created_at = %s AND user_idx = %s"
-    cursor.execute(query1, (id, log_created, user_idx))
-    result1 = cursor.fetchall()
+    query1 = "SELECT unit_name, status, member, equipment, supply FROM unit_init WHERE list_idx = %s AND created_at=%s AND user_idx=%s"
+    cursor.execute(query1, (id,log_created,user_idx))
+    result1=cursor.fetchall()
 
     # 추출한 데이터로부터 데이터프레임 생성
     dataframe1=pd.DataFrame(result1, columns=['unit_name', 'status', 'member', 'equipment', 'supply'])
 
     # 텍스트로 변환
     input_texts=[]
-    input_texts.append("해당 부대의 초기 상태 및 인원, 장비 수량")
-    input_texts.append("id \t unit_name \t status \t member \t equipment \t supply")
+    input_texts.append("해당 부대의 초기 상태 및 인원, 장비 수량\n")
+    input_texts.append("unit_idx \t unit_name \t status \t member(계급;무장;최대인원;현재인원) \t equipment(종류;장비명;최대수량;현재수량) \t supply(종류;품명;최대수량;현재수량)\n")
     for index, row in dataframe1.iterrows():
-        text=f"{id} \t {row['unit_name']} \t {row['status']} \t {row['member']} \t {row['equipment']} \t {row['supply']}"
+        text=f"{id} \t {row['unit_name']} \t {row['status']} \t {row['member']} \t {row['equipment']} \t {row['supply']}\n"
         input_texts.append(text)
 
     # event 파일에서 해당 부대 데이터 추출
-    query2 = "SELECT source_list_idx, target_list_idx, simulation_time, behavior_name, source_member, target_member, \
-        source_equipment, target_equipment, source_supply, target_supply, distance FROM event \
-            WHERE (source_list_idx = %s OR target_list_idx = %s) AND created_at=%s AND user_idx=%s"
-    cursor.execute(query2, (id,id, log_created, user_idx))
+    query2 = "SELECT source_list_idx, target_list_idx, simulation_time, source_member, target_member, \
+        source_equipment, target_equipment, source_supply, target_supply\
+              FROM event WHERE (source_list_idx = %s OR target_list_idx = %s) AND created_at=%s AND user_idx=%s"
+    cursor.execute(query2, (id,id,log_created,user_idx))
     result2=cursor.fetchall()
-
+    
     # 추출한 데이터로부터 데이터프레임 생성
-    dataframe2=pd.DataFrame(result2, columns=['source_list_idx', 'target_list_idx', 'simulation_time', 'behavior_name', 'source_member', \
-                                              'target_member', 'source_equipment', 'target_equipment', \
-                                              'source_supply', 'target_supply', 'distance' ])
-
-    input_texts.append("사격 이벤트 기록")
-    input_texts.append("source_unit_id\t target_unit_id \t simulation_time \t behavior_name \t source_member \t target_member \t \
-        source_equipment \t target_equipment \t source_supply \t target_supply \t distance")
+    dataframe2=pd.DataFrame(result2, columns=['source_list_idx', 'target_list_idx', 'simulation_time', 'source_member',\
+                                              'target_member', 'source_equipment', 'target_equipment',\
+                                                 'source_supply', 'target_supply'])
+    
+    # 데이터프레임의 길이
+    df_length = len(dataframe2)
+    input_texts.append("사격 이벤트 기록\n")
+    input_texts.append("source_list_idx\t target_list_idx \t simulation_time \n")
     for index, row in dataframe2.iterrows():
-        text=f"{row['source_list_idx']} \t {row['target_list_idx']} \t {row['simulation_time']} \t {row['behavior_name']} \
-            \t {row['source_member']}\t {row['target_member']}\t {row['source_equipment']}\t {row['target_equipment']}\
-            \t {row['source_supply']}\t {row['target_supply']}\t {row['distance']}"
+        if index != df_length - 1:
+            text=f"{row['source_list_idx']} \t {row['target_list_idx']} \t {row['simulation_time']}\n"
+        else:
+            text=f"{row['source_list_idx']} \t {row['target_list_idx']} \t {row['simulation_time']}\n"
+            input_texts.append(text)
+            input_texts.append("마지막 전투 결과\n")
+            if row['source_list_idx'] == id:
+                text = f"{row['simulation_time']} \t {row['source_member']} \t {row['source_equipment']} \t {row['source_supply']}\n"
+                input_texts.append("simulation_time \t member(계급;무장;최대인원;현재인원) \t equipment(종류;장비명;최대수량;현재수량) \t \
+                                   supply(종류;품명;최대수량;현재수량)\n")
+            else:
+                text = f"{row['simulation_time']} \t {row['target_member']} \t {row['target_equipment']} \t {row['target_supply']}\n"
+                input_texts.append("simulation_time \t member(계급;무장;최대인원;현재인원) \t equipment(종류;장비명;최대수량;현재수량) \t \
+                                   supply(종류;품명;최대수량;현재수량)\n")
         input_texts.append(text)
 
-    input_texts_str = "\n".join(input_texts)
-    return input_texts_str
+    # 전체 부대 id, 이름 목록 추출
+    query3= "SELECT list_idx, unit_name FROM unit_list WHERE simulation_time=%s AND user_idx=%s"
+    cursor.execute(query3, (log_created,user_idx))
+    result3=cursor.fetchall()
 
+    # 추출한 데이터로부터 데이터프레임 생성
+    dataframe3=pd.DataFrame(result3, columns=['list_idx', 'unit_name' ])
+    input_texts.append("전체 부대 목록\n")
+    input_texts.append("unit idx \t unit name\n")
+    for index, row in dataframe3.iterrows():
+        text=f"{row['list_idx']} \t {row['unit_name']}\n"
+        input_texts.append(text)
+
+    input_texts='\n'.join(input_texts)
+    return input_texts
 ###################################################################################################
 
 """
@@ -251,6 +296,12 @@ param4: 데이터베이스 커서
 return: 추출한 로그
 """
 def Extract_UnitBehavior(id, user_idx, log_created, cursor):
+    # 총 관측 시간 추출
+    query1="SELECT MAX(simulation_time) AS last_simulation_time FROM unit_behavior WHERE list_idx = %s AND created_at=%s AND user_idx=%s"
+    cursor.execute(query1,(id,log_created, user_idx))
+    time=cursor.fetchone()[0]
+
+    # 과업 수행 기록 추출
     query = "SELECT simulation_time, behavior_name, status FROM unit_behavior WHERE list_idx = %s AND created_at=%s AND user_idx=%s"
     cursor.execute(query, (id,log_created,user_idx))
     result=cursor.fetchall()
@@ -260,12 +311,16 @@ def Extract_UnitBehavior(id, user_idx, log_created, cursor):
 
     # 텍스트로 변환
     input_texts=[]
+    simulaition_time=f"총 관측 시간: {time}"
+    input_texts.append(simulaition_time)
     for index, row in dataframe.iterrows():
         text=f"SimulationTime: {row['SimulationTime']}, BehaviorName: {row['BehaviorName']}, Status: {row['Status']}"
         input_texts.append(text)
 
     input_texts='\n'.join(input_texts)
+    print(input_texts)
     return input_texts
+
 
 ###################################################################################################
 
@@ -374,7 +429,7 @@ return: 전처리 데이터 텍스트
 """
 def DataPreprocessing(openai, messages):
     chat_completion = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-0125",
         messages=messages
     )
 
@@ -401,24 +456,81 @@ def CreateMessage(characteristic, input_texts):
     messages = []
     if characteristic=="부대 이동 속도 / 위치 변화":  # 1
         messages = [
-            {
-                "role": "system",
-                "content": (
-                    "당신은 주어진 데이터를 분석에 용이한 형태로 전처리해야 합니다.\n"
-
-                )
-            },
+            {   "role": "system",
+                "content": ("당신은 주어진 데이터를 분석에 용이한 형태로 전처리해야 합니다.\n" )},
             {
                 "role": "user",
-                "content": "오직 주어진 데이터에서만 추출하고, 정확히 검토하라. 데이터는 다음 필드로 구성되어 있다: "
-                           "simulation time(시간 sec), positionLat(위도), positionLon(경도), positionAlt(고도), speed(이동 속도 km/h).\n"
-                           "반드시 첫 번째 데이터와 마지막 데이터를 포함하여, "
-                           "첫 번째 데이터와 마지막 데이터 사이의 simulation time을 기준으로 300초 간격의 데이터들을 추출하라."
+                "content": "데이터는 다음과 같은 필드로 이루어져 있습니다.. "
+                "simulation time(시간 sec), positionLat(위도), positionLon(경도), positionAlt(고도), speed(이동 속도 km/h).\n"
+                "첫 번째 행의 데이터와 마지막 행의 데이터 사이의 simulationTime을 기준으로 300초 간격의 데이터들을 추출하세요.\n"
+                "추출한 데이터에는 반드시 첫 번째 행의 데이터와 마지막 행의 데이터가 있어야 합니다.\n"
+                "다음 예시를 참고하여 형식에 맞춰 알려주세요.\n\n"
+                "이 예시에서 첫 번째 데이터는 simulaition time이 10일 때이고, 마지막 데이터는 simulation time이 3100일 때입니다.\n"
+                "결과는 다음과 같아야 합니다. 문장 없이 다음과 같은 형식으로만 답하세요.\n\n"
+                "10\t30.0\t100.0\t50.0\t2\n"
+                "310\t31.0\t120.0\t50.0\t2\n"
+                "610\t32.1\t100.0\t50.0\t2\n"
+                "910\t32.1\t100.0\t50.0\t2\n"
+                "1210\t32.1\t100.0\t50.0\t2\n"
+                "1510\t32.1\t100.0\t50.0\t2\n"
+                "1810\t32.1\t100.0\t50.0\t2\n"
+                "2110\t32.1\t100.0\t50.0\t2\n"
+                "2410\t32.1\t100.0\t50.0\t2\n"
+                "2710\t32.1\t100.0\t50.0\t2\n"
+                "3010\t32.1\t100.0\t50.0\t2\n"
+                "3100\t32.1\t100.0\t50.0\t2\n"
             },
-            {
-                "role":"assistant",
-                "content":(input_texts)
-            }
+            {   "role":"assistant","content": "전처리해야 하는 데이터:\n"+input_texts}
+        ]
+    elif characteristic=="인원/장비 수량 변화": #2
+        messages = [
+            {"role": "system", "content": "당신은 주어진 데이터를 분석에 용이한 형태로 전처리해야 합니다."},
+            {"role": "user", "content": 
+                "이 데이터는 순서대로 '해당 부대의 초기 상태 및 인원, 장비 수량', '사격 이벤트 기록', '마지막 전투 기록', '전체 부대 목록'이다.\n"
+                "'사격 이벤트 기록'에서 source는 공격의 주체이고 target은 공격의 대상이다. 해당 부대는 source일수도, target일 수도 있다. \n"
+                "'해당 부대의 초기 상태 및 인원, 장비 수량'을 이용하여 최대 값이 아닌 현재 값을 기준으로 초기 인원, 장비 수량을 명시하라.\n"
+                "'마지막 전투 기록'을 이용하여 최대 값이 아닌 현재 값을 기준으로 최종 인원, 장비 수량을 명시하라."
+                "'사격 이벤트 기록'에서 첫번째 simulation_time과 마지막 전투 기록의 simulaition_time을 이용하여 해당 부대가 전투를 치른 총 시간을 구하라."
+                "'사격 이벤트 기록'의 source_unit_id 또는 target_unit_id과 '전체 부대 목록'을 확인하여 전체 부대 중 해당 부대와 교전한 부대들을 목록으로 정리하라.\n"
+                "다음 예시를 보고 형식에 맞춰 답변하라.\n"
+                "만약 주어진 데이터가 다음과 같다면,\n\n"
+                "해당 부대의 초기 상태 및 인원, 장비 수량\n"
+                "unit_idx \t unit_name \t status \t member(계급;무장;최대인원;현재인원) \t equipment(종류;장비명;최대수량;현재수량) \t supply(종류;품명;최대수량;현재수량)\n"
+                "10 \t A-1-1 \t No Damage \t [{소/중위;소총;1;1};{하/중사;소총;5;5};{병;소총;20;20};{병;유탄발사기;5;5};{병;기관총;3;3}] \t "
+                "[{개인/공용화기;소총;50;50};{개인/공용화기;K201/M203;6;6};{개인/공용화기;K-3;3;3}] \t [{직사화기탄;소총탄;3600;3600};{직사화기탄;기관총탄;3000;3000};{직사화기탄;유탄발사기탄;90;90}]\n"
+                "사격 이벤트 기록\n"
+                "source_list_idx \t target_list_idx \t simulation_time\n"
+                "10 \t 5 \t 3000\n"
+                "6 \t 10 \t 3010\n"
+                "10 \t 6 \t 3015\n"
+                "10 \t 5 \t 3020\n"
+                "5 \t 10 \t 3040\n"
+                "마지막 전투 기록\n"
+                "simulation_time \t member(계급;무장;최대인원;현재인원) \t equipment(종류;장비명;최대수량;현재수량) \t supply(종류;품명;최대수량;현재수량)\n"
+                "3040 \t [{소/중위;소총;1;1};{하/중사;소총;5;5};{병;소총;20;10};{병;유탄발사기;5;0};{병;기관총;3;1}] \t "
+                "[{개인/공용화기;소총;50;30};{개인/공용화기;K201/M203;6;3};{개인/공용화기;K-3;3;3}] \t "
+                "[{직사화기탄;소총탄;3600;1000};{직사화기탄;기관총탄;3000;1000};{직사화기탄;유탄발사기탄;90;30}]\n"
+                "전체 부대 목록\n"
+                "1 \t B-1\n"
+                "5 \t B-13\n"
+                "6 \t B-14\n"
+                "(생략)\n\n"
+                "\n결과는 다음과 같아야 한다.\n\n"
+                "1. A-1-1 부대 초기 인원, 장비 수량:\n"
+                "- 인원: 소/중위(소총) 1명, 하/중사(소총) 5명, 병사(소총) 20명, 병사(유탄발사기) 5명, 병사(기관총) 3명\n"
+                "- 장비 수량: 소총 50개, K201/M203 6개, K-3 3개\n"
+                "- 자원품 수량: 소총탄 3600개, 기관총탄 3000개, 유탄발사기탄 90개\n\n"
+                "2. A-1-1 부대 최종 인원, 장비 수량:\n"
+                "- 인원: 소/중위(소총) 1명, 하/중사(소총) 5명, 병사(소총) 10명, 병사(유탄발사기) 0명, 병사(기관총) 1명\n"
+                "- 장비 수량: 소총 30개, K201/M203 3개, K-3 3개\n"
+                "- 자원품 수량: 소총탄 1000개, 기관총탄 1000개, 유탄발사기탄 30개\n\n"
+                "3. 전투 시간: \n"
+                "3000~3040초\n\n"
+                "4. A-1-1과 교전한 부대 목록:\n"
+                "- B-13\n"
+                "- B-14\n"
+            },
+            {"role": "assistant", "content": input_texts}
         ]
     elif characteristic == "부대의 전투력": # 3
         messages = [
@@ -453,14 +565,24 @@ def CreateMessage(characteristic, input_texts):
         messages = [
             {"role": "system", "content": "당신은 주어진 데이터를 분석에 용이한 형태로 전처리해야 합니다."},
             {"role": "user", "content": (
-                "Simulation Time과 Status를 이용하여 각 BehaviorName의 시작 시각과 종료 시각을 알려주세요. "
-                "다음 예시를 참고하여 형식에 맞춰 알려주세요. "
-                "BehaviorName: 전술기동\n"
-                "- 시작 시각: 60\n"
-                "- 종료 시각: 150\n"
-                "BehaviorName: 전술기동\n"
-                "- 시작 시각: 180\n"
-                "- 종료 시각: 9180"
+                "Simulation Time과 Status를 이용하여 모든 과업(Behavior Name)별로 시작 시각과 종료 시각을 알려주세요. "
+                "다음 예시를 참고하여 형식에 맞춰 알려주세요.\n\n"
+                "만약 주어진 데이터가 다음과 같다면,\n"
+                "총 관측 시간: 120000\n"
+                "SimulationTime: 30, BehaviorName: 점령, Status: Running\n"
+                "SimulationTime: 100, BehaviorName: 점령, Status: Finished\n"
+                "SimulationTime: 120, BehaviorName: 공격, Status: Running\n"
+                "SimulationTime: 300, BehaviorName: 공격, Status: Finished\n"
+                "결과는 아래와 같아야 합니다.\n"
+                "총 관측 시간: 120000\n"
+                "1. 과업명: 점령\n"
+                "- 시작 시각: 30\n"
+                "- 종료 시각: 100\n"
+                "- 소요 시간: 70\n"
+                "2. 과업명: 공격\n"
+                "- 시작 시각: 120\n"
+                "- 종료 시각: 300\n"
+                "- 소요 시간: 180\n"
             )},
             {"role": "assistant", "content": input_texts}
         ]
@@ -589,13 +711,9 @@ if __name__ == "__main__":
     else:
         name = None
 
-    id = FindID(name, user_idx, log_created, cursor)
+    
 
-#     print("Temp File Path:", temp_file_path)
-#     print("User ID:", user_idx)
-#     print("Log Created:", log_created)
-#     print("Characteristic:", characteristic)
-#     print("Name:", name)
+    id = FindID(name, user_idx, log_created, cursor)
 
     # ChatGPT Connect
     os.environ.get('OPENAI_API_KEY') is None
@@ -608,32 +726,50 @@ if __name__ == "__main__":
 
     if characteristic == "부대 이동 속도 / 위치 변화":
         input_texts=Extract_UnitSpeed(id, user_idx, log_created, cursor)
+        # 긴 데이터는 잘라서 넣기(3개로 나눔)
+        input_texts1, input_texts2, input_texts3=split_input_texts_3(input_texts)
+        input_texts1='\n'.join(input_texts1)
+        input_texts2='\n'.join(input_texts2)
+        input_texts3='\n'.join(input_texts3)
+        # chatGPT에도 3번에 나누어 전달
+        messages1=CreateMessage(characteristic, input_texts1)
+        preprocessed_data1=DataPreprocessing(openai, messages1)
+        if input_texts2!=None:
+            messages2=CreateMessage(characteristic, input_texts2)
+            preprocessed_data2=DataPreprocessing(openai, messages2)
+        if input_texts3!=None:
+            messages3=CreateMessage(characteristic, input_texts3)
+            preprocessed_data3=DataPreprocessing(openai, messages3)
+        # 완성된 3개의 전처리 결과를 합침
+        preprocessed_data="SimulationTime \t positionLat \t positionLon \t positionAlt \t speed\n"+preprocessed_data1+"\n"+preprocessed_data2+"\n"+preprocessed_data3
+    elif characteristic == "인원/장비 수량 변화":
+        input_texts=Extract_Event(id, user_idx, log_created, cursor)
         messages=CreateMessage(characteristic, input_texts)
+        preprocessed_data=DataPreprocessing(openai, messages)
     elif characteristic == "부대의 전투력":
         input_texts = Extract_Unit_CombatCapability(id, user_idx, log_created, cursor)
         messages = CreateMessage(characteristic, input_texts)
+        preprocessed_data=DataPreprocessing(openai, messages)
     elif characteristic == "부대의 피해 상황":
         input_texts = Extract_Unit_DamageStatus(id, user_idx, log_created, cursor)
         messages = CreateMessage(characteristic, input_texts)
+        preprocessed_data=DataPreprocessing(openai, messages)
     elif characteristic == "부대 행동":
         input_texts = Extract_UnitBehavior(id, user_idx, log_created, cursor)
         messages = CreateMessage(characteristic, input_texts)
+        preprocessed_data=DataPreprocessing(openai, messages)
     elif characteristic == "개체 탐지":
         input_texts = Extract_ObjectDetection(id, user_idx, log_created, cursor)
         converted_data = extract_data(input_texts)
         messages = CreateMessage(characteristic, converted_data)
+        preprocessed_data=DataPreprocessing(openai, messages)
     elif characteristic == "부대 정보":
         input_texts = Extract_info(user_idx, log_created, cursor)
         messages = CreateMessage(characteristic, input_texts)
+        preprocessed_data=DataPreprocessing(openai, messages)
     else:
         messages = []
         preprocessed_data=""
-
-    if characteristic=="인원/장비 수량 변화":
-        input_texts=Extract_Event(id, user_idx, log_created, cursor)
-        preprocessed_data=input_texts
-    else:
-        preprocessed_data=DataPreprocessing(openai, messages)
 
     # 전처리된 데이터를 작성할 경로
     output_file_path = os.path.join(os.getcwd(), "preprocessedData.txt")
